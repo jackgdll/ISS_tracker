0248f9857d67d58071ffe95c6f9fc2a580e11194
rosbif <jack.goodall@epitech.eu>
2021-12-22 18:15:13 +0100

    fix: order of args in pythag function was wrong

--- src/state/reducers/polyLineReducer.ts
+++ src/state/reducers/polyLineReducer.ts
@@ -1,11 +1,9 @@
 import { last, pythag } from "../../utils";
 import { PolyLineAction, PolyLineState, PolyLineTypes } from "../types";
 
-const DISTANCE_LIMIT = 1000;
+const DISTANCE_LIMIT = 150;
 
 const initialState: PolyLineState = [];
-
-const clone = (o: Object) => JSON.parse(JSON.stringify(o));
 
 const polyLineReducer = (state = initialState, action: PolyLineAction) => {
   switch (action.type) {
@@ -13,27 +11,36 @@ const polyLineReducer = (state = initialState, action: PolyLineAction) => {
       let lastLine: number[][] = last(state);
       const lastPoint: number[] = last(lastLine);
 
-      // Starts a new line if the ISS wrap arround the map
       if (state.length === 0) {
         return [[[action.payload.latitude, action.payload.longitude]]];
       }
+      const dist = pythag(
+        lastPoint[0],
+        lastPoint[1],
+        action.payload.latitude,
+        action.payload.longitude
+      );
+      if (dist > 50) {
+        console.log("dist: " + dist);
+      }
+      // Starts a new line if the ISS wraps arround the map
       if (
         state.length > 0 &&
         lastPoint.length === 2 &&
         pythag(
           lastPoint[0],
-          action.payload.latitude,
           lastPoint[1],
+          action.payload.latitude,
           action.payload.longitude
         ) > DISTANCE_LIMIT
       ) {
-        return clone(
+        return Object.assign([],
           state.concat([[[action.payload.latitude, action.payload.longitude]]])
         );
       } else {
         lastLine.push([action.payload.latitude, action.payload.longitude]);
         state[state.length - 1] = lastLine;
-        return clone(state);
+        return Object.assign([], state);
       }
 
     case PolyLineTypes.POLYLINE_CLEAR:
